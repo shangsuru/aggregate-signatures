79ae35d9625b14bd1d32a02465e24a14cf2caad6
shangsuru <helm.henry99@gmail.com>
2022-08-06 12:12:54 +0900

    Fix code formatting to abort commit when changes were made

--- .githooks/pre-commit.d/02_code-formatting.sh
+++ .githooks/pre-commit.d/02_code-formatting.sh
@@ -1,4 +1,9 @@
 echo "[githook] Running clang-format for code formatting"
-echo -n "Apply changes ..."
-clang-format -style=file -i src/*.cpp include/*.h tests/*.cpp
-echo "Done"
+
+clang-format --dry-run --Werror ${CXX_CHANGED_FILES}
+if [ $? -ne 0 ]; then 
+  echo -n "Apply changes ... "
+  clang-format -style=file src/*.cpp include/*.h tests/*.cpp
+  echo "Done"
+  exit 1
+fi
